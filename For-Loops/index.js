3bb7432af81f1197c1ef69acb2fa4697030cb4a2
Vishal Anand <vishalanand270@gmail.com>
2022-09-06 23:36:47 +0530

    For loops problem

---
+++ For-Loops/index.js
@@ -0,0 +1,183 @@
+// let bag="";
+// for(let i=1; i<=10; i++){
+//   bag+=i;
+// }
+// console.log(bag);
+// let bag = "";
+// for (let i = 1; i <= 5; i++) {
+
+//   if(i==5){
+//     continue;
+//   }
+//   bag = bag + i + " - ";
+// }
+// console.log(bag);
+
+// let bag = "";
+// for (let i = 1; i <= 5; i++) {
+//   bag = bag + i + " ";
+//   console.log(bag);
+// }
+
+// let bag ="";
+// for(let i=5; i>=1; i--){
+//   bag=bag+i+" ";
+// }
+// console.log(bag);
+
+// let bag ="";
+// let i=5;
+// while(i>=1){
+//   bag=bag+i+" ";  
+//   i--;
+// }
+// console.log(bag);
+
+// let fact=1;
+// for(let i=1; i<=5; i++){
+//   fact=fact*i;
+// }
+// console.log(fact);
+
+// let sum=0;
+// let count=0;
+// for(let i=1; i<=15; i++){
+//   if(i%2==1){
+//     sum=sum+i;
+//     count++;
+//   }
+// }
+//   console.log(sum/count);
+
+// for(let i=0; i<=10; i++){
+//   if(i==6){
+//     break;
+//   }
+//   console.log(i)
+// }
+// for(let i=1; i<=10; i++){
+//   if(i%4==0){
+//     break;
+//   }
+//   console.log(i*2)
+// }
+// let n=9;
+// let c1=0;
+// let c2=0;
+// for(let i=1; i<=9; i++){
+// if(n%i==0){
+//   c1++
+// }
+//   else{
+//     c2++
+//   }
+// }
+// console.log(c1,c2)
+
+// let x = 1;
+// let num = 0;
+
+// while (x <= 10) {
+
+//   if (x % 2 != 0) {
+
+//     num = 3 * x;
+//     if (num > 9) {
+//       console.log(num);
+//     }
+//   }
+//   x++;
+// }
+// console.log(x)
+
+// let x=15;
+// let n=1;
+// let c=0;
+// do{
+//   if(x%n==0){
+//     c++;
+//   }
+//   n++;
+// }
+//   while(n<=x)
+// console.log(c)
+
+// let x=15;
+// let n=1;
+// let c=0;
+// while(n<=x){
+//   if(x%n==0){
+//     c++
+//   }
+//   n++
+// }
+// console.log(c)
+
+// let a=64;
+
+// console.log(a**(1/2))
+
+// for(let i=3;i<=30;i+=3){
+//   console.log(i)
+// }
+
+// console.log(6%5)
+
+// let n=5;
+// let string="";
+
+// for(let i=0;i<n;i++){
+//   for(let j=0;j<n;j++){
+//     string+="*";
+//   }
+//   string+="\n";
+// }
+// console.log(string)
+
+// let string=""
+// for(let j=0;j<5;j++){
+//     string+="*";
+//   }
+// console.log(string)
+
+// let string = "";
+// for (let i = 0; i <= 5; i++) {
+//   for (let j = 0; j <= 5; j++) {
+//     if (i == 0 || i == 5) {
+//       string += "*";
+//     }
+//     else {
+//       if (j == 0 || j == 5) {
+//         string += "*";
+//       }
+//       else {
+//         string += " ";
+//       }
+//     }
+//   }
+//   string += "\n";
+// }
+// console.log(string)
+
+// let string = "";
+
+// for(let i=0;i<3;i++){
+//   for(let j=0;j<3;j++){
+//     if(i==0 || i==2){
+//       string=string+"*";
+//     }
+//     else{
+//       if(j=0 || j==2){
+//         string=string+"*";
+//       }
+//       else{
+//         string=string+" ";
+//       }
+//     }
+//   }
+//   string=string+"\n"
+// }
+// console.log(string);
+
+// let arr= ["vishal", "anand", "Rahul"]
+// console.log(arr[0])
