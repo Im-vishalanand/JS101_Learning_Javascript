8464ea24373a97a379113b60f61eb7ae917b27ec
Vishal Anand <vishalanand270@gmail.com>
2022-09-06 23:47:55 +0530

    Arrays removed

--- Arrays/problem_1.js
+++
@@ -1,6 +0,0 @@
-//Problem 1 : Given an array print the position (starting with 1) and the element in a single line.
-
-let arr = [5,7,34,"Aman","Anand","sam"];
-for (let i = 0; i <= arr.length-1; i++) {
-console.log(i, arr[i]);
-}
